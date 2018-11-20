4a374f52e010093d024b95884f62d34b78993762
98max1 <xa-fc@live.com.pt>
2018-11-20 22:48:51 +0000

    fiz npm install i run watch i deu,n sei se fica tudo aqui

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -74,3 +75,17 @@
     </div>
 </body>
 </html>
+=======
+@extends('master')
+
+@section('title', 'Vue.js App')
+
+@section('content')
+<router-link to="/users">Users</router-link>
+<router-link to="/list">List</router-link>
+<router-view></router-view>
+@endsection
+@section('pagescript')
+<script src="js/vue.js"></script>
+@stop  
+>>>>>>> Stashed changes
