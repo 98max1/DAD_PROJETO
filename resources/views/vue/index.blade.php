f7dfe94c39989de16bd577d8f229cda3261f2015
rukuyra <rubensamuel_jesus@hotmail.com>
2018-11-21 13:08:13 +0000

    interface

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -3,9 +3,13 @@
 @section('title', 'Vue.js App')
 
 @section('content')
-<router-link to="/users">Users</router-link>
-<router-link to="/list">List</router-link>
+
 <router-view></router-view>
+
+<nav class="navbar navbar-dark bg-dark">
+<router-link to="/users">Users</router-link>
+<router-link to="/list">List</router-link>  
+</nav>
 @endsection
 @section('pagescript')
 <script src="js/vue.js"></script>
