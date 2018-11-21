f317331528b1ecf917dec5babea10996d26018f9
rukuyra <rubensamuel_jesus@hotmail.com>
2018-11-21 13:47:28 +0000

    interface

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -4,12 +4,54 @@
 
 @section('content')
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Shift <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Notifications
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">All my notifications</a>
+            <a class="dropdown-item" href="#">Send notification</a>
+            </div>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Antonio
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Settings</a>
+            <a class="dropdown-item" href="#">Change password</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Logout</a>
+            </div>
+        </li>
+        </ul>
+    </form>
+  </div>
+</nav>
+
 <router-view></router-view>
 
-<nav class="navbar navbar-dark bg-dark">
-<router-link to="/users">Users</router-link>
-<router-link to="/list">List</router-link>  
-</nav>
 @endsection
 @section('pagescript')
 <script src="js/vue.js"></script>
